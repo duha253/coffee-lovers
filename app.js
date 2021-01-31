13c8ad2c49fccda5f5585073fe50e421a914f6cd
duha253 <duhayousef26@gmail.com>
2021-01-31 17:43:21 +0200

    do it2

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('Hello, its our unique page of coffee lovers' );
+var user = prompt('whats your age?' );
+
+if(user<15 ){
+    alert('We dont recommend getting too much caffeine' );
+}   else {
+    alert('welcome to become one of ours' );
+};
+    
+     
+        
+   
+
